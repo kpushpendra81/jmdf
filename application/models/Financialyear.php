710227427aa56facf9a4dba3c9d6f392f2af7ec6
Pushpendra Kumar <kpushpendra81@gmail.com>
2018-07-21 12:39:08 +0000

    change files

--- application/models/Financialyear.php
+++ application/models/Financialyear.php
@@ -1,21 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Logintable extends CI_Model {
+class Financialyear extends CI_Model {
 
 	/**
 	 * @param  [String]   username       [give by username from login form.]
 	 * @return [array]                 	 return login table Data getting from database according given username.
 	 */
-	function getLoginData($username) {
-
-		public function __construct() {
-        	parent::__construct();
-    	}
+	function getFsd() {
 		
-		// $this->db->select('password');
-		$this->db->where('username', $username);
-		$result = $this->db->get('login');
+		$result = $this->db->get('financialYear');
 		
 		/**
 		 * 	return login table Data getting from database according that username.
--- application/models/auth/Logintable.php
+++ application/models/auth/Logintable.php
@@ -1,15 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class FinancialYear extends CI_Model {
+class Logintable extends CI_Model {
+    
+    public function __construct() {
+    	parent::__construct();
+	}
 
 	/**
 	 * @param  [String]   username       [give by username from login form.]
 	 * @return [array]                 	 return login table Data getting from database according given username.
 	 */
-	function getFsd() {
+	function getLoginData($username) {
 		
-		$result = $this->db->get('financialYear');
+		// $this->db->select('password');
+		$this->db->where('username', $username);
+		$result = $this->db->get('login');
 		
 		/**
 		 * 	return login table Data getting from database according that username.
